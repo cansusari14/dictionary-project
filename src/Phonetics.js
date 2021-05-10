021391a53ea0673b948c8dc491aa03b2bac1f614
cansusari14 <63806074+cansusari14@users.noreply.github.com>
2021-05-10 22:52:52 +0200

    styling changes on results component

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,14 +1,15 @@
 import React from "react";
+import "./Phonetics.css";
 
 export default function Phonetics(props) {
   console.log(props.phonetic);
   return (
-    <div className="Phonetic">
+    <div className="Phonetics">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
       <br />
-      {props.phonetic.text}
+      <h2 className="text">{props.phonetic.text}</h2>
     </div>
   );
 }
