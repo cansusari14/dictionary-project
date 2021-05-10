7b815065e4bb19306e464e63f865f46f0af0b5ef
cansusari14 <63806074+cansusari14@users.noreply.github.com>
2021-05-10 17:28:22 +0200

    styling changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React, { useState } from "react";
 import "./App.css";
+import axios from "axios";
 import Dictionary from "./Dictionary";
+import Results from "./Results";
 
 function App() {
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+    setResults(response.data[0]);
+  }
+  function handleSearch(keyword) {
+    //documentation https://dictionaryapi.dev/
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
   return (
     <div className="App">
       <main>
-        <Dictionary />
-        <Dictionary />
+        <Dictionary onSearch={handleSearch} />
+        <Results results={results} />
       </main>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,28 +1,15 @@
 import React, { useState } from "react";
-import axios from "axios";
-import Results from "./Results";
 import "./Dictionary.css";
 
-export default function Dictionary() {
+export default function Dictionary(props) {
   let [keyword, setKeyword] = useState("");
-  let [results, setResults] = useState(null);
 
-  function handleResponse(response) {
-    console.log(response.data[0]);
-    console.log(response.data[0].meanings[0].definitions[0].definition);
-    setResults(response.data[0]);
-  }
-
-  function search(event) {
-    event.preventDefault();
-
-    //documentation https://dictionaryapi.dev/
-
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
-  }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.onSearch(keyword);
   }
 
   return (
@@ -45,10 +32,14 @@ export default function Dictionary() {
           just enter your desired word and surprise thee.
         </p>
         <p className="explanation">Type a word to search...</p>
-        <form onSubmit={search} onChange={handleKeywordChange}>
-          <input type="search" autoFocus="{true}" />
+        <form onSubmit={handleSubmit} onChange={handleKeywordChange}>
+          <input
+            type="search"
+            autoFocus="{true}"
+            placeholder="ex:dog,cat,hippopotamus"
+          />
+          <button>SUBMIT</button>
         </form>
-        <Results results={results} />
       </div>
     </div>
   );
